bade52418485ba3ab3d986743c2abd3bdea27b93
Lala Asanuma <lalacode0602@gmail.com>
2022-05-05 18:38:50 -0700

    first setup

---
+++ index.js
@@ -0,0 +1,44 @@
+// Required modules 
+const express = require("express");
+const app = express();
+// const dblib = require("./dblib.js");
+
+const multer = require("multer");
+const upload = multer();
+require('dotenv').config()
+
+// Add database package and connection string (can remove ssl)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
+// Setup EJS
+app.set("view engine", "ejs");
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Setup routes
+app.get("/", (req, res) => {
+    //res.send("Root resource - Up and running!")
+    res.render("index");
+});
